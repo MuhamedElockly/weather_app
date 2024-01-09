cfe41659ecb7e2373152116526e19d1e47314d62
dell <elocklymuhamed@gmail.com>
2024-01-09 21:26:04 +0200

    weathrer service exceptions

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -7,10 +7,17 @@ class WeatherService {
   WeatherService({required this.dio});
 
   Future<WeatherModel> getCurrentWeather({required String cityName}) async {
-    Response response = await dio.get(
-      'https://api.weatherapi.com/v1/forecast.json?key=ed6eef55ff2843dfa03144846240701&q=$cityName&days=1',
-    );
-    return WeatherModel.fromJson(response.data);
+    try {
+      Response response = await dio.get(
+        'https://api.weatherapi.com/v1/forecast.json?key=ed6eef55ff2843dfa03144846240701&q=$cityName&days=1',
+      );
+      return WeatherModel.fromJson(response.data);
+    } on DioException catch (e) {
+      String errorMessage = e.response?.data['error']['message'] ??
+          'Ops there was an error ,try later';
+      throw Exception(errorMessage);
+    } catch (e) {
+      throw Exception('Ops there was an error ,try later');
+    }
   }
 }
- 
